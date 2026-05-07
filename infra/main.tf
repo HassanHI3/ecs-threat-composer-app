aa5e6c1302e7ca70e002bfa36732bff7306623fe
HassanHI3 <Hassanhi3@icloud.com>
2026-05-07 17:45:01 +0100

    fnt change

--- infra/main.tf
+++ infra/main.tf
@@ -349,7 +349,7 @@ resource "aws_secretsmanager_secret" "my_secret" {
 }
 
 resource "aws_secretsmanager_secret_version" "my_secret_version" {
-  secret_id     = aws_secretsmanager_secret.my_secret.id
+  secret_id = aws_secretsmanager_secret.my_secret.id
   secret_string = jsonencode({
     AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY,
