81b0ec29596bc7af2fa3bd7c96be28b758d5a099
HassanHI3 <Hassanhi3@icloud.com>
2026-05-08 11:04:13 +0100

    fixes for dev.tf to variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,26 +1,31 @@
 variable "aws_region" {
   description = "AWS region to deploy resources into"
   type        = string
+  default = "eu-west-2"
 }
 
 variable "az_a" {
   description = "Primary availability zone"
   type        = string
+  default = "eu-west-2a"
 }
 
 variable "az_b" {
   description = "Secondary availability zone"
   type        = string
+  default = "eu-west-2b"
 }
 
 variable "environment" {
   description = "Environment name such as dev, staging, or prod"
   type        = string
+  default = "dev"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default = "10.0.0.0/16"
 }
 
 # variable "certificate_arn" {
@@ -32,11 +37,13 @@ variable "vpc_cidr" {
 variable "container_image" {
   description = "Full container image URI for the ECS task"
   type        = string
+  default = "threatmod-app-container-serverless"
 }
 
 variable "container_name" {
   description = "Name of the container in the ECS task definition"
   type        = string
+  default = "891377356090.dkr.ecr.eu-west-2.amazonaws.com/ecs-threat-composer-app:latest"
 }
 
 variable "container_image_tag" {
